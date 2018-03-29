63e69bfc519de1095814d060596a792de0b8666a
george39 <george35@utp.edu.co>
2018-03-29 15:52:03 -0500

    18 subir imagenes simultaneamente parte 1

--- propiedades/imagenes.php
+++ propiedades/imagenes.php
@@ -2,6 +2,7 @@
 <?php
 include '../extend/header.php';
 $id = htmlentities($_GET['id']);
+#para seleccionar la imagen
 $sel = $con->prepare("SELECT foto_principal FROM inventario WHERE propiedad = ?");
 $sel->bind_param('s', $id);
 $sel->execute();
@@ -12,7 +13,7 @@ if ($f = $res->fetch_assoc()) {
 ?>
 <!--codigo para actualizar la foto de perfil traido de materialize -->
 <div class="row">
-	<div class="col s12">
+	<div class="col s6">
     <h2 class="header">Actualizar foto principal</h2>
     <div class="card horizontal">
       <div class="card-image">
@@ -37,6 +38,33 @@ if ($f = $res->fetch_assoc()) {
         </div>        
       </div>
     </div>
+  </div>
+  <!--18 -->
+  <div class="col s6">
+      <h2 class="header">Cargar imagenes</h2>
+      <div class="row">
+        <div class="col s12">
+          <div class="card">
+            <div class="card-content">
+            <form action="ins_imagenes.php" method="post" enctype="multipart/form-data"> <!-- enctype="multipart/form-data" es para indicar que el formulario lleva un archivo -->
+          <input type="hidden" name="id" value="<?php echo $id ?>">
+          
+            <div class="file-field input-field">
+              <div class="btn">
+                <span>Foto</span>
+                <input type="file" name="ruta[]" multiple>
+              </div>
+              <div class="file-path-wrapper">
+                <input class="file-path validate" type="text" >
+              </div>
+            </div>
+            <button type="submit" class="btn">Guardar</button>
+          </form> 
+              
+            </div>
+          </div>
+        </div>
+      </div>
   </div>
 </div>
 
