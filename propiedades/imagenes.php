c1a9f694f584947ba711e8e9bfc0f4754b6ad90b
george39 <george35@utp.edu.co>
2018-03-29 20:32:24 -0500

    20 eliminar imagen

--- propiedades/imagenes.php
+++ propiedades/imagenes.php
@@ -95,7 +95,9 @@ $sel->close();
         $sel_img->execute();
         $res_img = $sel_img->get_result();
         while ($f_img = $res_img->fetch_assoc()) {?>
-          <img src="<?php echo $f_img['ruta'] ?>" width="300" heigth="400" >
+        <!--20 -->          
+           <a href="#"  onclick="swal({title: 'Esta seguro que desea eliminar la imagen?', text: 'Al eliminarla no podra recuperarlo', type: 'warning', showCancelButton: true, confirmButtonColor: '#3085d6', cancelButtonColor: '#d33', confirmButtonText: 'Si, eliminarla!' }).then(function () {
+                    location.href='eliminar_img.php?id=<?php echo $f_img['id'] ?>&ruta=<?php echo $f_img['ruta'] ?>&id_propiedad=<?php echo $id ?>'; })"><img src="<?php echo $f_img['ruta'] ?>" alt="" width="300" heigth="400" ></a>
           <?php 
         }
         $sel_img->close();
